2f84d081e7a4fde126eaf1748f8e343bf6006197
SimbolStudio <simbolStudio@gmail.com>
2016-08-27 23:07:03 +0800

    removed ISO3D property from modal and json

--- ProjectCurrencySurf/projectCurrencySurf/src/main/java/simbolstudio/projectcurrencysurf/model/Country.java
+++ ProjectCurrencySurf/projectCurrencySurf/src/main/java/simbolstudio/projectcurrencysurf/model/Country.java
@@ -6,7 +6,6 @@ package simbolstudio.projectcurrencysurf.model;
 public class Country {
     private String countryNm;
     private String countryISO2D;
-    private String countryISO3D;
     private String currencyCode;
 
     public String getCountryNm() {
@@ -15,10 +14,6 @@ public class Country {
 
     public String getCurrencyCode() {
         return currencyCode;
-    }
-
-    public String getCountryISO3D() {
-        return countryISO3D;
     }
 
     public String getCountryISO2D() {
