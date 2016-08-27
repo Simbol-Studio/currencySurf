475b244bb82a6acccfc3db34ba9b28065ece34d1
SimbolStudio <simbolStudio@gmail.com>
2016-08-27 21:22:37 +0800

    added loadJSONFromAsset function

--- ProjectCurrencySurf/projectCurrencySurf/src/main/java/simbolstudio/projectcurrencysurf/base/ui/BaseAppCompatActivity.java
+++ ProjectCurrencySurf/projectCurrencySurf/src/main/java/simbolstudio/projectcurrencysurf/base/ui/BaseAppCompatActivity.java
@@ -9,7 +9,18 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
+
 import simbolstudio.projectcurrencysurf.common.ConstantHelper;
+import simbolstudio.projectcurrencysurf.model.Country;
+import simbolstudio.projectcurrencysurf.model.CurrencyType;
 
 /**
  * Created by Marcus on 21-Aug-2016.
@@ -69,5 +80,37 @@ public abstract class BaseAppCompatActivity extends AppCompatActivity {
         snackbarTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, ConstantHelper.SNACKBAR_TEXT_SIZE);
         snackbarTextView.setMaxLines(ConstantHelper.SNACKBAR_TEXT_MAXLINE);
         snackbar.show();
+    }
+
+    public Object loadJSONFromAsset(String fileNm) {
+        String jsonString = null;
+        try {
+            InputStream is = getAssets().open(fileNm);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            jsonString = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        if (jsonString != null) {
+            Gson gson = new Gson();
+            try {
+                if (fileNm.equals(ConstantHelper.ASSETS_NAME_COUNTRIES)) {
+                    Type collectionType = new TypeToken<List<Country>>() {}.getType();
+                    List<Country> resultList = gson.fromJson(jsonString, collectionType);
+                    return resultList;
+                } else {
+                    Type collectionType = new TypeToken<List<CurrencyType>>() {}.getType();
+                    List<CurrencyType> resultList = gson.fromJson(jsonString, collectionType);
+                    return resultList;
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return null;
+            }
+        } else
+            return null;
     }
 }
