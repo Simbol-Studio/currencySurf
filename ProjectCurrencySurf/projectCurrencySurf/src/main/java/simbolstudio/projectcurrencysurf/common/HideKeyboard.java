f98d023117a85e1b140e4a97073c9b184c6e1dad
SimbolStudio <simbolStudio@gmail.com>
2016-08-31 13:45:08 +0800

    added hideKeyboard class

---
+++ ProjectCurrencySurf/projectCurrencySurf/src/main/java/simbolstudio/projectcurrencysurf/common/HideKeyboard.java
@@ -0,0 +1,16 @@
+package simbolstudio.projectcurrencysurf.common;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by Marcus on 28-Aug-2016.
+ */
+public class HideKeyboard {
+
+    public static void hideKeyboard(Context context, View view){
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+    }
+}
