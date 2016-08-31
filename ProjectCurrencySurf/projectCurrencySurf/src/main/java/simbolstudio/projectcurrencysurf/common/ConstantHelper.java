c68f4ce0096ef2d96a8c1e86b7c635e5759bf9ba
SimbolStudio <simbolStudio@gmail.com>
2016-08-31 13:36:08 +0800

    added new constant

--- ProjectCurrencySurf/projectCurrencySurf/src/main/java/simbolstudio/projectcurrencysurf/common/ConstantHelper.java
+++ ProjectCurrencySurf/projectCurrencySurf/src/main/java/simbolstudio/projectcurrencysurf/common/ConstantHelper.java
@@ -6,19 +6,18 @@ package simbolstudio.projectcurrencysurf.common;
 public class ConstantHelper {
     public static final int TIMEOUT_SECONDS = 10;
 
-    public static final String DEFAULT_BASE_CURRENCY="USD";
+    public static final String DEFAULT_BASE_CURRENCY_ID = "USD";
 
-//    key
-    public static final String KEY_ASSETS_NAME_COUNTRIES="countries.json";
-    public static final String KEY_ASSETS_NAME_CURRENCIES="currencies.json";
-    public static final String KEY_CURRENCY_RATE="KEY_CURRENCY_RATE";
-    public static final String KEY_SELECTED_CURRENCY="KEY_SELECTED_CURRENCY";
+    public static final String KEY_ASSETS_NAME_COUNTRIES = "countries.json";
+    public static final String KEY_ASSETS_NAME_CURRENCIES = "currencies.json";
+    public static final String KEY_ASSETS_NAME_FOREX = "forex.json";
+    public static final String KEY_FOREX_RATE = "KEY_FOREX_RATE";
 
-    public static final String SHARED_PREFERENCES="SHARED_PREFERENCES";
-    public static final String SHARED_PREFERENCES_BASE_CURRENCY="SHARED_PREFERENCES_BASE_CURRENCY";
-    public static final String SHARED_PREFERENCES_LAST_UPDATE="SHARED_PREFERENCES_LAST_UPDATE";
-    public static final String SHARED_PREFERENCES_CURRENCY_RATE_LIST="SHARED_PREFERENCES_CURRENCY_LIST";
-    public static final String SHARED_PREFERENCES_="SHARED_PREFERENCES_COUNTRY_ISO";
+    public static final String SHARED_PREFERENCES = "SHARED_PREFERENCES";
+    public static final String SHARED_PREFERENCES_BASE_CURRENCY_ID = "SHARED_PREFERENCES_BASE_CURRENCY_ID";
+    public static final String SHARED_PREFERENCES_LAST_UPDATE = "SHARED_PREFERENCES_LAST_UPDATE";
+    public static final String SHARED_PREFERENCES_CURRENCY_RATE_LIST = "SHARED_PREFERENCES_CURRENCY_LIST";
+    public static final String SHARED_PREFERENCES_SELECTED_CURRENCY_LIST = "SHARED_PREFERENCES_SELECTED_CURRENCY_LIST";
 
     public static final int SNACKBAR_TEXT_SIZE = 16;
     public static final int SNACKBAR_TEXT_MAXLINE = 3;
